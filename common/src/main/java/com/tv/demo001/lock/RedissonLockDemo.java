002895b662d2f05ff00f9b39b22ec1ed69516aee
hubo88 <hubo88@jd.com>
2022-11-07 17:42:49 +0800

    Redisson源码分析

---
+++ common/src/main/java/com/tv/demo001/lock/RedissonLockDemo.java
@@ -0,0 +1,52 @@
+package com.tv.demo001.lock;
+
+import java.io.File;
+import java.io.IOException;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+/**
+ * redisson
+ *
+ * @author hubo88
+ * @description
+ * @date 2022/11/6 8:23 PM
+ */
+public class RedissonLockDemo {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
+class Solution {
+
+    public void solution() throws IOException, InterruptedException {
+        // 1. Create config object
+        Config config = new Config();
+        config.useClusterServers()
+            // use "rediss://" for SSL connection
+            .addNodeAddress("redis://127.0.0.1:7181");
+
+        // or read config from file
+        config = Config.fromYAML(new File("config-file.yaml"));
+
+        // 2. Create Redisson instance
+
+        // Sync and Async API
+        RedissonClient redisson = Redisson.create(config);
+        RLock lock = redisson.getLock("lock");
+        lock.lock();
+        try{
+            Thread.sleep(300000);
+        }finally {
+            lock.unlock();
+        }
+
+    }
+
+}
