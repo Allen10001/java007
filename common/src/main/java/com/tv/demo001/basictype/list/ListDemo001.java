1c205d1e0c66ffa7a598c0371469c4cf4d2f5e08
hubo88 <hubo88@jd.com>
2023-04-20 18:50:33 +0800

    joiner

--- common/src/main/java/com/tv/demo001/basictype/list/ListDemo001.java
+++ common/src/main/java/com/tv/demo001/basictype/list/ListDemo001.java
@@ -1,6 +1,10 @@
 package com.tv.demo001.basictype.list;
 
 import com.google.common.collect.Lists;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Singleton;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,6 +15,17 @@ import java.util.List;
 public class ListDemo001 {
 
     public static void main(String[] args) {
+        Injector injector = Guice.createInjector();
+        // 即时绑定
+        Solution001 solution001 = injector.getInstance(Solution001.class);
+        solution001.resolve();
+    }
+}
+
+@Singleton
+class Solution002{
+
+    public void resolve() {
         List<List<Integer>> levelOrder = new LinkedList<List<Integer>>();
         levelOrder.add(0, Lists.newArrayList(1,2,3));
         levelOrder.add(0, Lists.newArrayList(2,2,3));
@@ -18,6 +33,16 @@ public class ListDemo001 {
         levelOrder.add(0, Lists.newArrayList(4,2,3));
         levelOrder.add(0, Lists.newArrayList(5,2,3));
         levelOrder.stream().forEach(list -> System.out.println(list.get(0)));
+    }
+}
+
+@Singleton
+class Solution001{
+
+    public void resolve() {
+        String ids = "12_13_14";
+        List<String> idList = Arrays.asList(ids.split("_"));
+        idList.forEach(System.out::println);
     }
 
 }
