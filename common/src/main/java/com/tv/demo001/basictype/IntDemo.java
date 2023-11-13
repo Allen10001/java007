0153c005dc004b756883414d3d663654844bcbfb
hubo88 <hubo88@jd.com>
2023-11-13 22:15:46 +0800

    temp

---
+++ common/src/main/java/com/tv/demo001/basictype/IntDemo.java
@@ -0,0 +1,16 @@
+package com.tv.demo001.basictype;
+
+/**
+ * int 测试
+ *
+ * @author hubo88
+ * @description
+ * @date 2023/11/13 7:41 PM
+ */
+public class IntDemo {
+
+    public static void main(String[] args) {
+        System.out.println((1f/2)*2);   // 1
+        System.out.println((1/2)*2);    // 0
+    }
+}
---
+++ common/src/main/java/com/tv/demo001/basictype/list/Solution003.java
@@ -0,0 +1,14 @@
+package com.tv.demo001.basictype.list;
+
+/**
+ * @author hubo88
+ * @description
+ * @date 2023/10/8 10:59 AM
+ */
+public class Solution003 {
+
+    public void solution() {
+
+    }
+
+}
